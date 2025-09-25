7748d4108178f614fd56c916c4c863ce98dc7d79
lizakein <lizakein@mail.ru>
2025-09-25 13:51:30 +0300

    feat: introduce unitsConfig file

---
+++ src/config/unitsConfig.ts
@@ -0,0 +1,26 @@
+export const UNITS_CONFIG = [
+  {
+    section: "Temperature",
+    ariaLabel: "Temperature units",
+    options: [
+      { label: "Celsius (°C)", value: "celsius" },
+      { label: "Fahrenheit (°F)", value: "fahrenheit" }
+    ]
+  },
+  {
+    section: "Wind Speed",
+    ariaLabel: "Wind Speed units",
+    options: [
+      { label: "km/h", value: "kmh" },
+      { label: "mph", value: "mph" }
+    ]
+  },
+  {
+    section: "Precipitation",
+    ariaLabel: "Precipitation units",
+    options: [
+      { label: "Millimeters (mm)", value: "mm" },
+      { label: "Inches (in)", value: "in" }
+    ]
+  },
+];
