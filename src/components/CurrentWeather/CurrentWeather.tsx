b49cc79ebfebbd75b32e373e3405c149377c1d7d
lizakein <lizakein@mail.ru>
2025-10-01 16:15:18 +0300

    refactor: improve CurrentWeather structure and codebase

--- src/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/CurrentWeather/CurrentWeather.tsx
@@ -1,3 +1,4 @@
+import type { HomeCity } from "../../types/homeCity";
 import type { UnitsState } from "../../types/unitsState";
 import type { WeatherData } from "../../types/weather";
 import { formatFullDate, formatNumber } from "../../utils/format";
@@ -7,18 +8,27 @@ import "./CurrentWeather.css";
 interface CurrentWeatherProps {
   data: WeatherData;
   units: UnitsState;
-  selectedCity: { 
-    lat: number, 
-    lon: number, 
-    city: string, 
-    country: string 
-  };
+  selectedCity: HomeCity;
 };
 
 export function CurrentWeather({ data, units, selectedCity }: CurrentWeatherProps) {
-  const currentData = data.current;
+  const { current } = data;
+  const { src, alt } = getWeatherIcon(current.weather_code);
 
-  const { src, alt } = getWeatherIcon(currentData.weather_code);
+  const details = [
+    { label: "Feels Like", 
+      value: `${formatNumber(current.apparent_temperature)}°` 
+    },
+    { label: "Humidity", 
+      value: `${formatNumber(current.relative_humidity_2m)}%` 
+    },
+    { label: "Wind", 
+      value: `${formatNumber(current.wind_speed_10m)} ${units.wind === "kmh" ? "km/h" : "mph"}` 
+    },
+    { label: "Precipitation", 
+      value: `${formatNumber(current.precipitation)} ${units.precipitation === "mm" ? "mm" : "in"}` 
+    }
+  ];
 
   return (
     <section className="weather" aria-label="Current weather">
@@ -27,38 +37,20 @@ export function CurrentWeather({ data, units, selectedCity }: CurrentWeatherProp
           <h2 className="weather__place">
             {`${selectedCity.city}, ${selectedCity.country}`}
           </h2>
-          <h3 className="weather__date">{formatFullDate(currentData.time)}</h3>
+          <p className="weather__date">{formatFullDate(current.time)}</p>
         </div>  
         <div className="weather__temperature-container">
           <img src={src} alt={alt} className="weather__icon"/>
-          <h3 className="weather__temperature">{formatNumber(currentData.temperature_2m)}°</h3>
+          <h3 className="weather__temperature">{formatNumber(current.temperature_2m)}°</h3>
         </div> 
       </header>
       <dl className="weather__details">
-        <div className="weather__detail">
-          <dt className="weather__label">Feels Like</dt>
-          <dd className="weather__value">{formatNumber(currentData.apparent_temperature)}°</dd>
-        </div>
-        <div className="weather__detail">
-          <dt className="weather__label">Humidity</dt>
-          <dd className="weather__value">{currentData.relative_humidity_2m}%</dd>
-        </div>
-        <div className="weather__detail">
-          <dt className="weather__label">Wind</dt>
-          <dd className="weather__value">
-            {formatNumber(currentData.wind_speed_10m)} 
-            &nbsp;
-            {units.wind === "kmh" ? "km/h" : "mph"}
-          </dd>
-        </div>
-        <div className="weather__detail">
-          <dt className="weather__label">Precipitation</dt>
-          <dd className="weather__value">
-            {formatNumber(currentData.precipitation)} 
-            &nbsp;
-            {units.precipitation === "mm" ? "mm" : "in"}
-          </dd>
-        </div>
+        { details.map(({label, value}) => (
+          <div className="weather__detail">
+            <dt className="weather__label">{label}</dt>
+            <dd className="weather__value">{value}</dd>
+          </div>
+        ))}
       </dl>
     </section>
   );
